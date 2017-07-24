e6300bda7bf89bcb6404d8fc18351617e330590b
sagar <sagar.bhatt@rtcamp.com>
2017-07-24 17:40:23 +0530

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,60 @@ var path = require( 'path' );
 var webpack = require( 'webpack' );
 
 var config = {
-	devtool: 'source-map',
-	entry:{
-		home: './js/home/main.js',
-		customizer:'./js/customizer/main.js',
-	    base: [ './js/post/main.js',
-		'./js/page/main.js',
-		'./js/single/main.js',
-		'./js/archive/main.js'
-		]
+ 	devtool: 'eval-source-map',
+    entry:{
+        home: './js/home/main.js',
+        customizer:'./js/customizer/main.js',
+        base: [ './js/post/main.js',
+        './js/page/main.js',
+        './js/single/main.js',
+        './js/archive/main.js'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: '[name].bundle.js',
+        sourceMapFilename: '[file].map'
+    },
+    externals: {
+        'jquery' : 'jQuery'
+    },
+    module: {
+	rules: [
+		{
+			test: /\.vue$/,
+			loader: 'vue-loader',
+			options: {
+			  loaders: {
+			  }
+			  // other vue-loader options go here
+			}
+		},
+		{
+			test: /\.js$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/
+		},
+		{
+			test: /\.(png|jpg|gif|svg)$/,
+			loader: 'file-loader',
+			options: {
+			  name: '[name].[ext]?[hash]'
+			}
+		}
+	]
+  	},
+	resolve: {
+		alias: {
+		  'vue$': 'vue/dist/vue.esm.js'
+		}
 	},
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		publicPath: '/dist/',
-		filename: '[name].bundle.js',
-		sourceMapFilename: '[file].map'
+	devServer: {
+		noInfo: true
 	},
-	externals: {
-		'jquery' : 'jQuery'
+	performance: {
+		hints: false
 	}
 };
 
