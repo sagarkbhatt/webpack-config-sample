025bbf80c1bcb7f2c63fcb45f8e525fbda172696
sagar <sagar.bhatt@rtcamp.com>
2017-07-25 12:24:38 +0530

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -78,7 +78,7 @@ module.exports = {
 	]
 };
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'dev-test') {
-	module.exports.devtool = '#source-map';
+	module.exports.devtool = 'source-map';
 	// http://vue-loader.vuejs.org/en/workflow/production.html
 	module.exports.plugins = (module.exports.plugins || []).concat([
 		new webpack.DefinePlugin({
