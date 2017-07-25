f5a4da85424311bec9fefb04ecdcfc23539f2688
sagar <sagar.bhatt@rtcamp.com>
2017-07-25 15:36:01 +0530

    Add blank theme structure

---
+++ js/customizer/main.js
@@ -0,0 +1,2 @@
+import { customizer } from './customizer';
+customizer();
--- js/default/main.js
+++ js/default/main.js
@@ -1,6 +1,4 @@
 import { navigation } from './navigation';
 navigation();
-import { customizer } from './customizer';
-customizer();
 import { skip_link_focus_fix } from './skip-link-focus-fix';
 skip_link_focus_fix();
